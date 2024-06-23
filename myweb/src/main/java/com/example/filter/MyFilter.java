87f7f6a644ae9a425f99708b63443413599f79d5
yangguangyin <ilike.yanggy@gmail.com>
2024-06-23 20:28:04 +0800

    demo

--- myweb/src/main/java/com/example/filter/MyFilter.java
+++ myweb/src/main/java/com/example/filter/MyFilter.java
@@ -3,18 +3,18 @@ package com.example.filter;
 import javax.servlet.*;
 import java.io.IOException;
 
-public class AuthFilter implements Filter {
+public class MyFilter implements Filter {
 
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("AuthFilter-----init");
+        System.out.println("MyFilter-----init");
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("AuthFilter-----doFilter");
+        System.out.println("MyFilter-----doFilter");
         chain.doFilter(request,response);
     }
 
     public void destroy() {
-        System.out.println("AuthFilter----destroy");
+        System.out.println("MyFilter----destroy");
     }
 }
--- myweb/src/main/java/com/example/servlet/MyServlet.java
+++ myweb/src/main/java/com/example/servlet/MyServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class HelloServlet extends HttpServlet {
+public class MyServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
